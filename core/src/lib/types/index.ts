36517529508c3a7a699b24f58b56e4e6dfa30b3f
Manuel Rivera <menu.rivera@gmail.com>
2024-04-02 22:18:24 -0600

    [MOD] -> moved src/types/ to src/lib/types/; added index.ts

---
+++ core/src/lib/types/index.ts
@@ -0,0 +1,4 @@
+export * from './signer.type'
+export * from './getSigner.types'
+export * from './series.types'
+export * from './provider.type'
---
+++ core/src/lib/types/series.types.ts
@@ -0,0 +1,23 @@
+interface TokenMetadata {
+	title: string,
+	description: string,
+	media: string,
+	copies?: string,
+}
+
+export interface JsonToken {
+	series_id: number,
+	owner_id: string,
+	metadata: TokenMetadata,
+	price: number,
+}
+
+export interface JsonSerie {
+	series_id: number,
+	metadata: TokenMetadata,
+	royalty?: any,
+	price?: number,
+	owner_id: string,
+	content_id: string,
+}
+
