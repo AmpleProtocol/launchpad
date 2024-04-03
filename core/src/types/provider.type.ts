393f00dbfd38b1bf566d550e36317f06664f8b06
Manuel Rivera <menu.rivera@gmail.com>
2024-04-02 22:18:56 -0600

    [DEL] -> types/ and lib/utils/ bc of refactorization

--- core/src/types/provider.type.ts
+++
@@ -1,6 +0,0 @@
-export interface IProvider {
-	/**
-		* Upload a new tokengated asset
-	*/
-	upload(): Promise<any>,
-}
--- core/src/types/signer.type.ts
+++
@@ -1,8 +0,0 @@
-import { FinalExecutionOutcome } from "@near-wallet-selector/core";
-import { IContractMethodParams } from "./getSigner.types";
-
-export interface ISigner {
-	address: string,
-	view<T = any>(props: IContractMethodParams): Promise<T>,
-	call<T = any>(props: IContractMethodParams): Promise<T | void | FinalExecutionOutcome>,
-}
