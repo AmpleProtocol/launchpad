74d54891c3ac78ffe8a90720a9b05c954bc7aeb1
Manuel Rivera <menu.rivera@gmail.com>
2024-04-08 19:54:51 -0600

    [MOD] -> nitro.config.ts

--- server/nitro.config.ts
+++ server/nitro.config.ts
@@ -1,4 +1,12 @@
 //https://nitro.unjs.io/config
 export default defineNitroConfig({
-	srcDir: "server"
+	srcDir: "server",
+	experimental: {
+		tasks: true,
+	},
+	scheduledTasks: {
+		// trigger server/tasks/analytics.ts task every 24 hours
+		'0 0 * * *': ['analytics']
+	}
 });
+
