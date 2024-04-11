f8d64714a1adf9f708d79ecda6d9b6e6f608502f
Manuel Rivera <menu.rivera@gmail.com>
2024-04-11 01:10:34 -0600

    [ADD] -> livepeer provider

--- core/src/lib/types/provider.type.ts
+++ core/src/lib/types/provider.type.ts
@@ -1,11 +1,44 @@
+import { DetailedError } from "tus-js-client"
+
+export interface IJwtProps {
+	jwtPrivateKey: string,
+	jwtPublicKey: string,
+	referenceId: string,
+	issuer: string,
+	metadata: any
+}
+
 export interface IProvider {
 	/**
 		* Upload a new tokengated asset
 	*/
-	upload(): Promise<any>,
+	upload(
+		title: string,
+		filename: string,
+		asset: File,
+		onError?: (error: Error | DetailedError) => void,
+		onProgress?: (bytesUploaded: number, byptesTotal: number) => void,
+		onSuccess?: () => void
+	): Promise<any>,
+
 	/**
 		* Retrieves viewcount for a given content
 		* @param referenceId - Reference of the content for viewership metrics to be retrieved from
 	*/
-	retrieveViewcount(referenceId: string, from?: Date, to?: Date): Promise<number>
+	retrieveViewcount(
+		referenceId: string,
+		from?: Date,
+		to?: Date
+	): Promise<number>,
+
+	/**
+		* Creates a jwt for tokengated access 
+	*/
+	createJwt(
+		jwtPrivateKey: string,
+		jwtPublicKey: string,
+		referenceId: string,
+		issuer: string,
+		metadata: any
+	): Promise<string>
 }
