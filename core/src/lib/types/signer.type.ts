bed66daf1f7d7e94a86c7edf19a33e55723f852f
Manuel Rivera <menu.rivera@gmail.com>
2024-04-30 18:29:31 -0600

    [MOD] -> sign() and verifySignature() added to ISigner and getSigner.ts utils

--- core/src/lib/types/signer.type.ts
+++ core/src/lib/types/signer.type.ts
@@ -2,7 +2,18 @@ import { FinalExecutionOutcome } from "@near-wallet-selector/core";
 import { IContractMethodParams } from "./getSigner.types";
 
 export interface ISigner {
+	/** Signer address */
 	address: string,
+
+	/** Isomorphic view function call */
 	view<T = any>(props: IContractMethodParams): Promise<T>,
+
+	/** Isomorphic function call */
 	call(props: IContractMethodParams): Promise<void | FinalExecutionOutcome>,
+
+	/** Signs the given message and returns the signature as string */
+	sign(message: string, callbackUrl?: string): Promise<string | void>,
+
+	/** Verifies that the signature corresponds to the message */
+	verifySignature(message: string, signature: string, callbackUrl?: string): Promise<boolean>
 }
