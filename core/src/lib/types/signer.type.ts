b3beff02b73d8a30ba98e1dde94153a40ae79869
Manuel Rivera <menu.rivera@gmail.com>
2024-06-20 18:08:01 -0600

    [MOD] -> getSigner modded, added testing packages to core

--- core/src/lib/types/signer.type.ts
+++ core/src/lib/types/signer.type.ts
@@ -3,17 +3,11 @@ import { IContractMethodParams } from "./getSigner.types";
 
 export interface ISigner {
 	/** Signer address */
-	address: string,
+	accountId: string,
 
 	/** Isomorphic view function call */
 	view<T = any>(props: IContractMethodParams): Promise<T>,
 
 	/** Isomorphic function call */
 	call(props: IContractMethodParams): Promise<void | FinalExecutionOutcome>,
-
-	/** Signs the given message and returns the signature as string */
-	sign(message: string, callbackUrl?: string): Promise<string | void>,
-
-	/** Verifies that the signature corresponds to the message */
-	verifySignature(message: string, signature: string, callbackUrl?: string): Promise<boolean>
 }
