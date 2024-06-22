459afd7ebced0693825a7b6080edfa15fcac9019
Manuel Rivera <menu.rivera@gmail.com>
2024-06-22 17:10:25 -0600

    [ADD] -> analytics cron implemented

--- server/server/plugins/database.ts
+++ server/server/plugins/database.ts
@@ -1,15 +1,31 @@
-export default defineNitroPlugin(async nitroApp => {
+// import { v4 as uuid } from 'uuid'
+export default defineNitroPlugin(async () => {
 	const db = useDatabase()
 	// assert tables are created 
 
-	// CONTENTS table
-	const res = await db.sql`CREATE TABLE IF NOT EXISTS contents (
-		"id" TEXT PRIMARY KEY, 
-		"title" TEXT, 
-		"collectionId" INT, 
-		"playbackId" TEXT,
-		"assetId" TEXT
+	// todo: remove this 
+	await db.sql`drop table analytics`
+	await db.sql`drop table contents`
+	console.log('[db]: dropped tables')
+
+	// contents table
+	await db.sql`CREATE TABLE IF NOT EXISTS contents (
+		id TEXT PRIMARY KEY, 
+		title TEXT, 
+		collectionId INT, 
+		playbackId TEXT,
+		assetId TEXT
 	)`;
-	if (res.error) throw new Error(res.error)
-	console.log('[db]: contents table ready')
+
+	// analytics table
+	await db.sql`CREATE TABLE IF NOT EXISTS analytics (
+		id TEXT PRIMARY KEY,
+		timestamp INT,
+		streams INT,
+		contentId TEXT,
+		FOREIGN KEY (contentId) REFERENCES contents(id)
+	)`;
+
+
+	console.log('[db]: tables ready')
 })
--- server/server/tasks/analytics.ts
+++ server/server/tasks/analytics.ts
@@ -1,12 +1,41 @@
+import { v4 as uuid } from 'uuid'
+
 export default defineTask({
 	meta: {
 		name: "analytics",
 		description: "Upload analytics to the treasury"
 	},
 	async run({ payload, context }) {
+		console.log(`[cron:analytics]: cron task started`)
+		const db = useDatabase()
+		const { treasury } = await useContracts()
+
 		// 1. Retrieve every content there are 
-		// 2. Retrieve analytics for each content from provider (livepeer)
-		// 3. Upload analytics to treasury
+		const { rows: contents } = await db.sql`SELECT * FROM contents`;
+
+		const timestamp = Date.now()
+
+		for (const content of contents) {
+			const lastAnalyticRes = await db.sql`SELECT * FROM analytics WHERE contentId = ${content.id} ORDER BY timestamp DESC LIMIT 1`
+
+			const fromDate = lastAnalyticRes.rows[0]
+				? new Date(lastAnalyticRes.rows[0].timestamp as number)
+				: undefined
+
+			// 2. Retrieve analytics for each content from provider (livepeer)
+			const streams = await livepeer.retrieveViewcount(content.playbackId as string, fromDate)
+
+			// 3. Upload analytics to treasury
+			await treasury.addAnalyticsData(content.id as string, streams, timestamp)
+
+			await db.sql`INSERT INTO analytics VALUES (
+				${uuid()}, 
+				${timestamp},
+				${streams},
+				${content.id}
+			)`
+		}
+
 		return { result: true }
 	}
 })
