b592cd53a36b0ac23c67cb72134a93cfb225721b
Manuel Rivera <menu.rivera@gmail.com>
2024-09-17 14:07:53 -0600

    [ADD:server] -> /api/analytics-task only available on dev

--- server/server/api/analytics-task.get.ts
+++ server/server/api/analytics-task.get.ts
@@ -1,4 +1,6 @@
 export default eventHandler(async (event) => {
+	if (process.env.NODE_ENV != 'development') return 'Not available on production'
+
 	const { result } = await runTask('analytics')
 	return { result }
 })
